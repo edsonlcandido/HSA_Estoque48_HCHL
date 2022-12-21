9f7a6daaa5fb43207030e32ce8289f184181eedf
candided <edson.candido@howden.com>
2022-12-21 13:00:16 -0300

    add usuario presenter

---
+++ HSA_Estoque48/Presenter/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSA_Estoque.Presenter
+{
+    internal class Usuario : Model.IUsuario
+    {
+        public int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Nome { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool isAdmin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
