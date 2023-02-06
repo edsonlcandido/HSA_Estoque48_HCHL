b4a9a1930df295e843aead91fb615c641cbb1998
candided <edson.candido@howden.com>
2023-02-06 13:21:43 -0300

    include suport to migrations

---
+++ HSA_Estoque48/Migrations/202212191900-create table produtos.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE IF NOT EXISTS produtos (
+    id               TEXT    DEFAULT (0) 
+                             PRIMARY KEY
+                             UNIQUE,
+    descricao        TEXT    DEFAULT ('PRODUTO SEM CADASTRO'),
+    quantidadeMinima REAL    DEFAULT (0),
+    quantidadeMaxima REAL    DEFAULT (0),
+    quantidadeTotal  REAL    DEFAULT (0),
+    leadTime         INTEGER DEFAULT (0),
+    tipo             TEXT    DEFAULT ('TBD'),
+    unidade          TEXT    DEFAULT ('TBD'),
+    localizacao      TEXT    DEFAULT ('ALMOXARIFADO'),
+    caixa            TEXT    DEFAULT ('TBD'),
+    obs              TEXT    DEFAULT ('') 
+);
