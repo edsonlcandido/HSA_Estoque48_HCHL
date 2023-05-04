0cc081fccc8308cdb4711a44097fa4e0d3997663
edsonlcandido <edson.luiz.candido@gmail.com>
2023-05-03 23:19:45 -0300

    fix migrations

---
+++ HSA_Estoque48/Migrations/202212201900-create table historicos.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE IF NOT EXISTS historico (
+    dataMovimentacao      DATE,
+    usuario               STRING,
+    quantidadeMovimentada DOUBLE,
+    tipoMovimentacao      STRING,
+    produtoId             INTEGER,
+    solicitante           STRING,
+    pedidoCentroCusto     STRING,
+    notaFiscal            STRING,
+    obs                   STRING
+);
---
+++ HSA_Estoque48/Migrations/202212211901-create table unidades.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE IF NOT EXISTS unidades (
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT,
+    visible INTEGER
+);
